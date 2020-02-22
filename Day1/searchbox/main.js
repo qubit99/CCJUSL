9792204169b4b2a472b77886d6af1edae1e7bd62
qubit99 <architranjan9@gmail.com>
2020-02-22 11:05:53 +0530

    Day 1

---
+++ Day1/searchbox/main.js
@@ -0,0 +1,36 @@
+let students = [
+    {
+        "name" : "Mridul",
+        "Desc" : "Former intern at Aukland university"
+    },
+    {
+        "name" : "Mahak",
+        "Desc" : "Don't be mistaken by the name, she is a boy."
+    },
+    {
+        "name" : "Aniswar",
+        "Desc" : "Makes sexist memes and then posts it on FB."
+    },
+    {
+        "name" : "Manan",
+        "Desc" : "loves hanging out with friends doesn't care for socio exam."
+    },
+    {
+        "name" : "Shatadru",
+        "Desc" : "Master of python, but can't handle his own python."
+    },
+    {
+        "name" : "TMCP",
+        "Desc" : "Congrats man, you defeated NOTA!"
+    },
+    {
+        "name" : "Sayak",
+        "Desc": "Long distance is not my thing so I leave it at \"It's complicated\""
+    },
+    {
+        "name" : "Margot Robbie",
+        "Desc": "Talented and hot &hearts;"
+    },
+
+]
+
